75c8f49f566bc0fe36e71af4d3f5c8cba8ff3592
Catherine Badart <cbadart@DESKTOP-0V8P6TL.localdomain>
2018-05-10 23:23:09 -0400

    finished functionality for modal buttons

--- start_selling/static/start_selling/scripts/sales-grid.js
+++ start_selling/static/start_selling/scripts/sales-grid.js
@@ -49,22 +49,18 @@ function populateModal(id) {
 	details.rows[1].cells[1].innerHTML = "$"+Number(item.unit_price).toFixed(2)+" per "+item.unit;
 	var pic = document.getElementById("modal-pic")
 	pic.src = item.img_src;
-	console.log(cart);
-	console.log(id);
+	document.getElementById("inputId").value = item.id;
+	//console.log(cart);
+	//console.log(id);
 	if(id in cart) {
-		details.rows[2].cells[1].innerHTML = "<input value=\""+cart[id]+"\">";
+		details.rows[2].cells[1].innerHTML = "<input id=\"input-quant\" value=\""+cart[id]+"\">";
 		document.getElementById("rmBtn").classList.remove("disabled");
 	} else {
-		details.rows[2].cells[1].innerHTML = "<input value=\"0\">";
+		details.rows[2].cells[1].innerHTML = "<input id=\"input-quant\" value=\"0\">";
 		document.getElementById("rmBtn").classList.add("disabled");
 	}
 }
 
-function dragProduce(event, id) {
-	event.target.style.opacity = "0.4";
-	console.log("started dragging an item");
-	increaseItemQuant(id);
-}
 function increaseItemQuant(id) {
 	var AlreadyInCart;
 	if(!(id in cart)) {
@@ -137,17 +133,61 @@ function updateCartTable(id, AlreadyInCart){
 	}
 	changeTotal();
 }
+
+function removeFromModal() {
+	var id = Number(document.getElementById("inputId").value);
+	var cartTable = document.getElementById('cart-table');
+	var itemRow = document.getElementById('cart-item-'+id);
+	var i = itemRow.parentNode.parentNode.rowIndex;
+	cartTable.deleteRow(i);
+	delete cart[id];
+	sessionStorage.removeItem(id);
+}
+
+function addFromModal() {
+	var id = Number(document.getElementById("inputId").value);
+	var quant = Number(document.getElementById('input-quant').value);
+	var item = mydata[id];
+	sessionStorage.setItem(id, quant);
+	var total = quant * item.unit_price;
+	if(id in cart) {
+		cart[id] = quant;
+		var itemRow = document.getElementById('cart-item-'+id);
+		if(quant > 0) {
+			itemRow.cells[4].innerHTML = "<input class=\"cart-quant\" value=\""+quant+"\"/><button class=\"btn btn-default up-btn\" onclick=\"increaseItemQuant("+id+")\"><i class=\"fas fa-angle-up\"></i></button><button class=\"btn btn-default down-btn\" onclick=\"decreaseItemQuant("+id+")\"><i class=\"fas fa-angle-down\"></i></button>";
+			itemRow.cells[6].innerHTML = "<b>$"+Number(total).toFixed(2)+"</b>";
+		}
+	} else {
+		var table = document.getElementById('cart-table');
+		var row = table.insertRow(-1);
+		var c1 = row.insertCell(0); //img
+		var c2 = row.insertCell(1); //produce name
+		var c3 = row.insertCell(2); //unit price
+		var c4 = row.insertCell(3); //times
+		var c5 = row.insertCell(4); //quantity
+		var c6 = row.insertCell(5); //equals
+		var c7 = row.insertCell(6); //total price
+		
+		c1.innerHTML = "<img class=\"cart-item-img\" src=\""+item.img_src+"\">";
+		c2.innerHTML = item.item_name;
+		c3.innerHTML = "$"+Number(item.unit_price).toFixed(2);
+		c4.innerHTML = "x";
+		c5.innerHTML = "<input class=\"cart-quant\" value=\""+quant+"\"/><button class=\"btn btn-default up-btn\" onclick=\"increaseItemQuant("+id+")\"><i class=\"fas fa-angle-up\"></i></button><button class=\"btn btn-default down-btn\" onclick=\"decreaseItemQuant("+id+")\"><i class=\"fas fa-angle-down\"></i></button>";
+		c6.innerHTML = "=";
+		c7.innerHTML = "<b>$"+Number(total).toFixed(2)+"</b>";
+		
+		var row_id = "cart-item-" + id;
+		row.id = row_id;
+		cart[id] = quant;
+	}
+	changeTotal();
+	
+}
 function removeItemfromCart(id){
 	var cartTable = document.getElementById('cart-table');
 	var itemRow = document.getElementById('cart-item-'+id);
 	var i = itemRow.parentNode.parentNode.rowIndex;
 	cartTable.deleteRow(i);
-}
-
-function dragEnter(event){
-	if(event.target.className == "dropzone"){
-		console.log("item dropped in cart");
-	}
 }
 //dragula([document.getElementsByClassName("produce-img"), document.getElementById("cart-items")], {
 //  copy: true
